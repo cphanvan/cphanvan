d0cf455436e2edd065c59726412f028ccf960980
cathphanvan <catherine.phanvan@gmail.com>
2022-03-29 15:44:35 +0200

    build locally to gh_pages

---
+++ gh_pages/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2022/03/22/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "Échos graphiques",
+        "excerpt":"Aurélie et Nicolas attendent un heureux événement. Ils viennent passer Noël au calme dans un village isolé du Haut-Doubs. Mais quels échos du passé leur présence va-t-elle y réveiller ?   Le recueil Creepy Christmas and bloody New Year est en vente sur le site des éditions BeetleBooks Publishing.  ","categories": [],
+        "tags": [],
+        "url": "/publications/echos-graphiques/",
+        "teaser": "/assets/images/publications/echos-graphiques-teaser.jpg"
+      },{
+        "title": "Enfants des neiges",
+        "excerpt":"Marre des contes de Noël qui ne font qu’égrener tous les codes du genre ? Enfants des neiges, nouvelle d’une vingtaine de pages, est un (non-)conte hivernal, atypique et garanti 100% sans Père Noël, qui vous fera voyager en compagnie de deux enfants et de leur grand-mère sur les traces...","categories": [],
+        "tags": [],
+        "url": "/publications/enfants-des-neiges/",
+        "teaser": "/assets/images/publications/enfants-des-neiges-teaser.png"
+      },{
+        "title": "Évasion nocturne",
+        "excerpt":"Pourquoi est-elle là ? Pourquoi reste-t-elle immobile ? Elle réalise qu’elle ne le sait pas. Alors, elle s’évade…   Vous pouvez lire la courte nouvelle Évasion nocturne gratuitement sur le site du magazine Pas vu, pas lu.        ","categories": [],
+        "tags": [],
+        "url": "/publications/evasion-nocturne/",
+        "teaser": "/assets/images/publications/evasion-nocturne-teaser.png"
+      },{
+        "title": "Le cadeau des fées",
+        "excerpt":"Avec son cancer, elle a tout perdu : ses espoirs, ses rêves, son compagnon. Mais on dit que les fées rôdent, lors de la nuit du solstice… Le recueil Solstice d’été est disponible : directement sur le site des Éditions Inusitées (paiement en dollars canadiens) en version numérique sur le site...","categories": [],
+        "tags": [],
+        "url": "/publications/le-cadeau-des-fees/",
+        "teaser": "/assets/images/publications/le-cadeau-des-fees-teaser.jpg"
+      },{
+        "title": "Les pipistrelles de Gabriel",
+        "excerpt":"Gabriel a emménagé dans une vieille maison isolée, séduit par ses futures colocataires. Mais ces dernières semblent bien vouloir lui délivrer un message…   Le recueil de nouvelles de l’édition 2021 peut être téléchargé gratuitement sur le site du Festival du Fantastique de Béziers.  ","categories": [],
+        "tags": [],
+        "url": "/publications/les-pipistrelles-de-gabriel/",
+        "teaser": "/assets/images/publications/les-pipistrelles-de-gabriel-teaser.jpg"
+      },{
+        "title": "Plein Sud",
+        "excerpt":"Un tweet récent d’une personnalité provoque la colère d’une jeune femme, passionnée de jeux vidéos au point d’avoir choisi d’en faire son métier. Elle ne s’attendait pas à ce que lui réservent les coulisses de ce milieu professionnel… La newsletter littéraire Réticule #11 sur le thème New Game+ est disponible...","categories": [],
+        "tags": [],
+        "url": "/publications/plein-sud/",
+        "teaser": "/assets/images/publications/plein-sud-teaser.jpg"
+      }]
