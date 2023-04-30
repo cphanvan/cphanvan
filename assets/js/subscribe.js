2ce2078c41ea6e4db95233f21d6a7ed8224cca14
cathphanvan <catherine.phanvan@gmail.com>
2023-04-30 13:19:02 +0200

    update newsletter subscription forms

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -1,16 +1,28 @@
 function subscribe(form) {
-	let email = form.querySelector('#email')?.value;
+    document.querySelector('#error-message')?.classList?.add('hidden');
+    document.querySelector('#success-message')?.classList?.add('hidden');
+    form.querySelector('#validation-message')?.classList?.add('hidden');
+    form.querySelectorAll('.entry__error').forEach((el) => { 
+        el.innerHTML = '';
+    });
+    let email = form.querySelector('#EMAIL')?.value;
 	if (!validateEmail(email)) {
-		alert("adresse mail invalide");
+        form.querySelector('#validation-message>span').innerHTML = ' L\'adresse email est invalide.';
+        form.querySelector('#validation-message')?.classList?.remove('hidden');
 		return false;
 	}
-	let firstName = form.querySelector('#firstName')?.value || '';
-	form.querySelector('#ack')?.classList?.remove("hidden");
-	form.querySelector('#email').value = '';
+	let firstName = form.querySelector('#PRENOM')?.value || '';
+    let url = `${form.getAttribute("action")}?isAjax=1`;
+	form.querySelector('#EMAIL').value = '';
 	if (firstName !== '') {
-		form.querySelector('#firstName').value = '';
+		form.querySelector('#PRENOM').value = '';
 	}
-	postContact(email, firstName);
+    const fd = new FormData();
+    fd.append('EMAIL', email);
+    fd.append('PRENOM', firstName);
+    fd.append('email_address_check', '');
+    fd.append('locale', 'fr');
+	postContact(fd, url);
 	return false;
 }
 
@@ -18,31 +30,38 @@ function validateEmail(email) {
 	return /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
 }
 
-function postContact(email, firstName) {
-	const apiUrl = `https://sfyyfdj2go4pngumrojdy4nyfy0mwdyu.lambda-url.eu-west-3.on.aws/`;
-	const request = new Request(
-		apiUrl,
-		{
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				"email": email,
-				"firstName": firstName
-			})
-	});
-	fetch(request)
-	.then(response => {
-		if (response.status !== 200) {
-			alert("Une erreur s'est produite lors de votre inscription. Je vous suggère d'utiliser le navigateur Chrome. Si le problème persiste, n'hésitez pas à me contacter par email.")
-		} else {
-			alert("Votre demande d'inscription a bien été prise en compte. Pour la confirmer, veuillez cliquer sur le lien dans le mail que vous allez recevoir.")
-		}
-	}).
-	catch(error => {
-		alert("Une erreur s'est produite lors de votre inscription. Je vous suggère d'utiliser le navigateur Chrome. Si le problème persiste, n'hésitez pas à me contacter par email.")
-	});
+function postContact(fd, url) {
+	const xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.onload = (e) => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                document.querySelector('#error-message')?.classList?.add('hidden');
+                document.querySelector('#success-message')?.classList?.remove('hidden');
+                window.open('/confirmation.html', '_blank');
+            } else if (xhr.status === 400) {
+                document.querySelector('#success-message')?.classList?.add('hidden');
+                document.querySelector('#error-message')?.classList?.add('hidden');
+                const errors = JSON.parse(xhr.response)?.errors;
+                if (errors?.EMAIL) {
+                    document.querySelector('#EMAIL').nextElementSibling.innerHTML = errors?.EMAIL;
+                }
+                if (errors?.PRENOM) {
+                    document.querySelector('#PRENOM').nextElementSibling.innerHTML = errors?.PRENOM;
+                }
+                if (errors?.captcha) {
+                    document.querySelector('#sib-captcha').nextElementSibling.innerHTML = 'La vérification du captcha a échoué.';
+                }
+            } else {
+                document.querySelector('#success-message')?.classList?.add('hidden');
+                document.querySelector('#error-message')?.classList?.remove('hidden');
+            }
+        }
+    };
+    xhr.onerror = (e) => {
+        document.querySelector('#success-message')?.classList?.add('hidden');
+        document.querySelector('#error-message')?.classList?.remove('hidden');
+    };
+    xhr.send(fd);
 	return false;
 }
